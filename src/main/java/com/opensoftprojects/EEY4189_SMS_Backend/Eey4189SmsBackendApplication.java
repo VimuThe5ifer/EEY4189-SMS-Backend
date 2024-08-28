fd352c40140d4f1ae1b709f6decb55957b62570d
Vimukthi Sampath Charles <vimuthe5ifer@gmail.com>
2024-08-28 11:38:19 +0530

    Initial commit for the project

---
+++ src/main/java/com/opensoftprojects/EEY4189_SMS_Backend/Eey4189SmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.opensoftprojects.EEY4189_SMS_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eey4189SmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eey4189SmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensoftprojects/EEY4189_SMS_Backend/Eey4189SmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensoftprojects.EEY4189_SMS_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eey4189SmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
